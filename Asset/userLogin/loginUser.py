6673fb2062cb58601d96b20fdc4f04884c6816d6
deway29 <dedewahyu1996@gmail.com>
2025-02-12 13:53:35 +0700

    update commit

--- Asset/userLogin/loginUser.py
+++ Asset/userLogin/loginUser.py
@@ -2,23 +2,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from Asset.userLogin.sourceLogin import resource
 import time
 
+locator = resource()
+
 def login(driver):
-    driver.find_element(By.XPATH, "//div[@class='panel header']//a[contains(.,'Sign In')]").click()
-    email_input = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.XPATH, "//span[@class='base']"))
+    driver.find_element(By.XPATH, locator.find_element).click()
+    WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, locator.page))
     )
-    email_input = driver.find_element(By.XPATH, "//input[@id='email']")
+    email_input = driver.find_element(By.XPATH, locator.email_input)
     email_input.send_keys("JimCarry@icloud.com") 
     time.sleep(2)
-    element_hover = driver.find_element(By.XPATH, "//a[contains(.,'Notes')]")
+    element_hover = driver.find_element(By.XPATH, locator.element_hover)
     ActionChains(driver).move_to_element(element_hover).perform()
     time.sleep(2)
-    passwordemail_input = driver.find_element(By.XPATH, "//input[@name='login[password]']")
-    passwordemail_input.send_keys("@tested123") 
+    password_input = driver.find_element(By.XPATH, locator.password_input)
+    password_input.send_keys("@tested123") 
     time.sleep(2)
-    driver.find_element(By.XPATH, "//button[@class='action login primary']/span[.='Sign In']").click()
+    driver.find_element(By.XPATH, locator.signin).click()
     time.sleep(5)
     driver.quit()
 
---
+++ Asset/userLogin/sourceLogin.py
@@ -0,0 +1,7 @@
+class resource:
+    find_element = "//div[@class='panel header']//a[contains(.,'Sign In')]"
+    page = "//span[@class='base']"
+    email_input = "//input[@id='email']"
+    element_hover = "//a[contains(.,'Notes')]"
+    password_input = "//input[@name='login[password]']"
+    signin = "//button[@class='action login primary']/span[.='Sign In']"
